408428309067512b3adeb867c35e7ed5f786b2be
Tusharvermaaa <tusharverma8433@gmail.com>
2025-06-10 02:00:54 +0530

    client server connected successfully

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express= require("express");
-const http= require("http");
-const path= require("path");
-const app=express();
-const PORT=9000;
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+const PORT = 9000;
+const { Server } = require("socket.io");
+const server = http.createServer(app); // this is http server , handles express js
+const io = new Server(server);
+app.use(express.static(path.resolve("./public/")));
 
-const server=http.createServer(app);
-app.use(express.static(path.resolve("./public/")))
+app.get("/", (req, res) => {
+  return res.sendFile("./index.html");
+});
 
-app.get("/", (req, res)=>{
-    return res.sendFile("./index.html");
-})
+io.on("connection", (socket) => {
+  socket.on("a message", (message) => {
+    console.log("a new user message , ", message); // prints in vs code console (which is basically a server for my development time   )
+    io.emit("message", message);
+  });
+  console.log("socket is client , ", socket.id);
+});
 
-server.listen(PORT,()=>{console.log("server started at prot : " , PORT)})
+server.listen(PORT, () => {
+  console.log("server started at prot : ", PORT);
+});
+//  note all the http request are handled by express js ,
+//  and
